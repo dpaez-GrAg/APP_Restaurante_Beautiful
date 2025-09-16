d95d88278d245719f44f2728b5bc6f3484460e9f
dpaezm <diegopaezmacias@gmail.com>
2025-09-16 12:29:52 +0200

    commit para rebase

--- supabase/migrations/20250902150532_9f8261a2-48c1-4426-aebd-2689521f2582.sql
+++
@@ -1,8 +0,0 @@
-
--- Añadir time slots de cena hasta las 22:45 solo si no existen
-INSERT INTO public.time_slots (time, max_capacity)
-SELECT v.t::time, 50
-FROM (VALUES ('22:00'), ('22:15'), ('22:30'), ('22:45')) AS v(t)
-WHERE NOT EXISTS (
-  SELECT 1 FROM public.time_slots ts WHERE ts.time = v.t::time
-);
